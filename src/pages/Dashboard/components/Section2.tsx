31f9e9d755d53a17668651d9fc10c96753b23894
mubeess <mubarakibrahim2015@gmail.com>
2023-04-10 11:47:57 +0100

    done

--- src/pages/Dashboard/components/Section2.tsx
+++ src/pages/Dashboard/components/Section2.tsx
@@ -9,15 +9,20 @@ import { RootState } from "../../../redux/store";
 import { FieldArray, Formik, FormikProvider, useFormik } from "formik";
 import Button from "../../../components/Button";
 import { setEducatioanlQualification } from "../../../redux/user/userDetailSlice";
+import Loading from "../../../components/Loading";
+import Alert from "../../../components/Alert";
 interface initialStateProp {
   initial: [any] | any;
 }
 interface Sec2Props {
   onSend: (val: any) => void;
 }
+
 const initalState: initialStateProp["initial"] = [];
 function Section2() {
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const [msg, setMsg] = useState("");
   const initialValues = {
     qualification: [
       {
@@ -31,114 +36,119 @@ function Section2() {
   const formik = useFormik({
     initialValues,
     onSubmit: (val) => {
+      setLoading(true);
       dispatch(
         setEducatioanlQualification({
           educationalQualification: [...val.qualification],
         })
       );
+      setMsg("Data saved");
+      setTimeout(() => {
+        setLoading(false);
+        setMsg("");
+      }, 1000);
     },
   });
 
   // const user = useSelector((user: RootState) => user.userDetail);
 
-  useEffect(() => {
-    return () => {
-      formik.handleSubmit();
-    };
-  }, []);
   return (
     <>
-    <div className="section2_main">
-      <Header text="SECTION B: Educational Qualification" />
-      <FormikProvider value={formik}>
-        <div className="section2_container">
-          <FieldArray
-            name="qualification"
-            render={(arrayHelpers) => {
-              const qualifications = formik.values.qualification;
-              return (
-                <>
-                  {qualifications.length > 0
-                    ? qualifications.map((item, index) => (
-                        <div className="sec2_inputs" key={index.toString()}>
-                          <Input
-                            {...formik.getFieldProps(
-                              `qualification.${index}.institution`
-                            )}
-                            onChange={formik.handleChange}
-                            style={{
-                              width: window.innerWidth>746?"30%":"100%",
-                            }}
-                            outlined
-                            label="Institution Attend "
-                            placeholder="Enter the name of institution attend"
-                          />
-                          <Input
-                            {...formik.getFieldProps(
-                              `qualification.${index}.from`
-                            )}
-                            onChange={formik.handleChange}
-                            outlined
-                            type="date"
-                            label="From"
-                          />
-                          <Input
-                            {...formik.getFieldProps(
-                              `qualification.${index}.to`
-                            )}
-                            onChange={formik.handleChange}
-                            outlined
-                            type="date"
-                            label="To"
-                          />
-                          <Input
-                            {...formik.getFieldProps(
-                              `qualification.${index}.qualificationObtained`
-                            )}
-                            onChange={formik.handleChange}
-                            outlined
-                            type="text"
-                            label="Qualification Obtained"
-                          />
-                          {index == qualifications.length - 1 && (
-                            <AddButton
-                              onClick={() =>
-                                arrayHelpers.push({
-                                  institution: "o",
-                                  from: "2",
-                                  to: "2",
-                                  qualificationObtained: "2",
-                                })
-                              }
+      <div className="section2_main">
+        <Loading loading={loading} />
+        <Alert text={msg} />
+        <Header text="SECTION B: Educational Qualification" />
+        <FormikProvider value={formik}>
+          <div className="section2_container">
+            <FieldArray
+              name="qualification"
+              render={(arrayHelpers) => {
+                const qualifications = formik.values.qualification;
+                return (
+                  <>
+                    {qualifications.length > 0
+                      ? qualifications.map((item, index) => (
+                          <div className="sec2_inputs" key={index.toString()}>
+                            <Input
+                              {...formik.getFieldProps(
+                                `qualification.${index}.institution`
+                              )}
+                              onChange={formik.handleChange}
+                              style={{
+                                width: window.innerWidth > 746 ? "30%" : "100%",
+                              }}
+                              outlined
+                              label="Institution Attend "
+                              placeholder="Enter the name of institution attend"
                             />
-                          )}
-                          {index !== qualifications.length - 1 && (
-                            <DeleteButton
-                              onClick={() => arrayHelpers.remove(index)}
+                            <Input
+                              {...formik.getFieldProps(
+                                `qualification.${index}.from`
+                              )}
+                              onChange={formik.handleChange}
+                              outlined
+                              type="date"
+                              label="From"
                             />
-                          )}
-                        </div>
-                      ))
-                    : null}
-                </>
-              );
-            }}
-          />
-          {/* <Button
+                            <Input
+                              {...formik.getFieldProps(
+                                `qualification.${index}.to`
+                              )}
+                              onChange={formik.handleChange}
+                              outlined
+                              type="date"
+                              label="To"
+                            />
+                            <Input
+                              {...formik.getFieldProps(
+                                `qualification.${index}.qualificationObtained`
+                              )}
+                              onChange={formik.handleChange}
+                              outlined
+                              type="text"
+                              label="Qualification Obtained"
+                            />
+                            {index == qualifications.length - 1 && (
+                              <AddButton
+                                onClick={() =>
+                                  arrayHelpers.push({
+                                    institution: "o",
+                                    from: "2",
+                                    to: "2",
+                                    qualificationObtained: "2",
+                                  })
+                                }
+                              />
+                            )}
+                            {index !== qualifications.length - 1 && (
+                              <DeleteButton
+                                onClick={() => arrayHelpers.remove(index)}
+                              />
+                            )}
+                          </div>
+                        ))
+                      : null}
+                  </>
+                );
+              }}
+            />
+            {/* <Button
                 onClick={() => console.log(values, "===")}
                 style={{ width: "30%", marginBottom: 20 }}
                 label="Save"
               /> */}
-              
-        </div>
-      </FormikProvider>
-      
-    </div>
-    <Button style={{
-        width:70
-      }} onClick={()=>{
-        formik.handleSubmit()
-      }} label="Add Data"/>
+          </div>
+        </FormikProvider>
+      </div>
+      <Button
+        style={{
+          width: 70,
+        }}
+        onClick={() => {
+          formik.handleSubmit();
+        }}
+        label="Add Data"
+      />
     </>
   );
 }
